7837d8efb124ecbc815996d32bf2e83bec494947
vineesh <vineeshw1994@gmail.com>
2025-10-14 00:12:41 +0530

    update the vite config file

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,21 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
   server: {
     port: 3000,
-  }
+  },
+
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase limit to avoid unnecessary warnings
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+        },
+      },
+    },
+  },
 });
